5013131a9576701742ea1174d18a50c7f501b9da
Prajwal Kulkarni <kulkarniprajwal.01@gmail.com>
2023-07-24 00:29:07 +0530

    Creating entities

--- src/main/java/railway/booking/app/AppApplication.java
+++ src/main/java/railway/booking/app/AppApplication.java
@@ -2,15 +2,11 @@ package railway.booking.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
 
-@EntityScan("railway.booking.entities")
-@ComponentScan(basePackages = "railway.booking.*")
 @OpenAPIDefinition(info = @Info(title = "Railway Booking System", license = @License(name = "railway.booking"), version = "1.0", description = "A simple railway booking app"))
 @SpringBootApplication
 public class AppApplication {
